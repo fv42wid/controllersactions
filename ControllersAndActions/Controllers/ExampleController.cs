eb91e56e289e610fc9a2c4f6cca26ee52f4e64e5
Frank Vinci <fv42wid@yahoo.com>
2017-04-01 19:56:02 +0000

    completed

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public VirtualFileResult Index() =>
+            File("/lib/bootstrap/dist/css/boostrap.css", "text/css");
+
+        public ViewResult Result() => View((object)"Hello World");
+
+        public RedirectToActionResult Redirect() =>
+            RedirectToAction("Index");
+    }
+}
---
+++ ControllersAndActions/Infrastructure/CustomHtmlResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ControllersAndActions.Infrastructure
+{
+    public class CustomHtmlResult : IActionResult
+    {
+        public string Content { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 22;
+            context.HttpContext.Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII.GetBytes(Content);
+            return context.HttpContext.Response.Body.WriteAsync(content, 0, content.Length);
+        }
+    }
+}
